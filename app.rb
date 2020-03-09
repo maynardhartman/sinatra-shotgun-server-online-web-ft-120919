bb7536ee1d49e51ab43b1b91a75bcff6341f2dc1
Maynard Hartman <mjhartman@brighthouse.com>
2020-03-09 13:33:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
+<<<<<<< HEAD
     "Welcome to your app!!!! My Server is running on port "
+=======
+    "Welcome to your app!!!! I BUILT THIS!"
+>>>>>>> 1f27a1302c04bd6009bc33f293e95568f0c68bd7
   end
 
 end
